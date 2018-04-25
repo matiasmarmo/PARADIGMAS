b69b7ec8cb75d0aaef326828e4769882c38b51e4
matias <matumarmo@gmail.com>
2018-04-25 15:04:27 -0300

    inicio proyecto

---
+++ jorge.hs
@@ -0,0 +1,31 @@
+funcionMisteriosa1 f = f 2 True
+
+funcionMisteriosa2 g = g . length
+
+funcionMisteriosa3 h g x = h (g x) True 
+
+
+
+
+darVuelta f1 a b = f1 b a
+
+componer f1 f2 a= f1(f2 a)
+
+esMultiploDe a = ((==0).(mod a)) 
+
+esBisiesto a =((||(esMultiploDe 16 a)).(esMultiploDe 400)) a
+
+--jorge = map snd 
+tuitCorto a = (length (snd a))
+aux a = (tuitCorto a)<10
+
+--segundo = (csv).(map snd) 
+
+sumarSegun f a = foldl1 (+) (map f a)
+
+exists f a = (map f a)
+
+--cuandoHabloMas a |"reducido" =length(fst a)<=length(snd a)
+--                 |"normal" = otherwise
+
+cuandoHizoMasLlamadasBreves a |length(filter (==True)(map (<=2) (fst(a))))<=length(filter (==True)(map (<=2) (snd(a))))="reducido" |otherwise="normal"
